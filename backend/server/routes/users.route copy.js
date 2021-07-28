0c208776a5d86eeccb54be5235fe40d553403b8b
ElZee <bobbyjackson13@live.com>
2021-07-28 15:47:53 -0500

    added user model

---
+++ backend/server/routes/users.route copy.js
@@ -0,0 +1,10 @@
+const UsersController = require('../controllers/Users.controller')
+
+
+module.exports = app => { 
+    app.get('/api/users', UsersController.findAllUsers)
+    app.get('/api/users/:_id/', UsersController.findOneMonster)
+    app.post('/api/users/create/', UsersController.createMonster) 
+    app.patch('/api/users/:_id/update/', UsersController.updateMonster)
+    app.delete('/api/users/:_id/delete/', UsersController.deleteMonster)
+}
