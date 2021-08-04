39f7238d6b2e0b63859b352cf6541eb44e9f6135
TelevisionNinja <TelevisionNinja@gmail.com>
2021-08-04 02:29:34 -0500

    loading order and typo fix

--- backend/server/models/dens.model.js
+++ backend/server/models/dens.model.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
+// load monster model first
+require('./monsters.model');
 const Schema = mongoose.Schema
 
 const DensSchema = new Schema({
     serverId: String,
-    monsters:[{ typr: Schema.Types.ObjectId, ref: 'Monsters' }]
+    monsters:[{ type: Schema.Types.ObjectId, ref: 'Monsters' }]
 })
 
 const Dens = mongoose.model('Dens', DensSchema);
