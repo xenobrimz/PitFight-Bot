cf6e29f2d8c10cd547fdf5c69d7781cb691fc212
TelevisionNinja <TelevisionNinja@gmail.com>
2021-08-07 01:09:48 -0500

    fixed names

--- backend/server/routes/names.route.js
+++ backend/server/routes/names.route.js
@@ -3,8 +3,8 @@ const NamesController = require('../controllers/names.controller')
 
 module.exports = app => { 
     app.get('/api/names', NamesController.findAllNames)
-    app.get('/api/names/:_id/', Names.findOneDen)
-    app.post('/api/names/create/', Names.createDen) 
-    app.patch('/api/names/:_id/update/', Names.updateDen)
-    app.delete('/api/names/:_id/delete/', Names.deleteDen)
+    app.get('/api/names/:_id/', NamesController.findOneName)
+    app.post('/api/names/create/', NamesController.createName) 
+    app.patch('/api/names/:_id/update/', NamesController.updateName)
+    app.delete('/api/names/:_id/delete/', NamesController.deleteName)
 }
