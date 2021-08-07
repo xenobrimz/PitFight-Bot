65b8d5db1979eb1580b2115b1f1c004aca87a1f3
TelevisionNinja <TelevisionNinja@gmail.com>
2021-08-07 01:08:57 -0500

    updated schema

--- backend/server/models/names.model.js
+++ backend/server/models/names.model.js
@@ -4,20 +4,17 @@ const Schema = mongoose.Schema
 const NamesSchema = new Schema({
     _id: String,
     names:{
-        monsterNames:[
-            {
-                first:[    
-                ],
-                middle:[
-                ],
-                last:[
-                ]
-            }
-        ],
-
+        monsterNames:{
+            first:[
+            ],
+            middle:[
+            ],
+            last:[
+            ]
+        },
         skillNames:[
-        ]   
-    }      
+        ]
+    }
 })
 
 const Names = mongoose.model('Names', NamesSchema);
