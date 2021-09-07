fae3188f4d3a4f6dcf5db2fc586f87bf9dcf9c02
TelevisionNinja <TelevisionNinja@gmail.com>
2021-09-07 00:28:46 -0500

    added names

--- backend/server.js
+++ backend/server.js
@@ -9,6 +9,7 @@ app.use(express.json(), express.urlencoded({extended: true}));
 require('./server/routes/monsters.route')(app);
 require('./server/routes/dens.route')(app);
 require('./server/routes/users.route')(app);
+require('./server/routes/names.route')(app);
 
 app.listen(port, ()=>console.log(`we running on port ${port}. Aaaaeeeeyyyy!!!`));
 
