dbc225c456f04464c18a23b7faf21c17c58a571b
TelevisionNinja <TelevisionNinja@gmail.com>
2021-09-07 00:36:31 -0500

    testing functions

--- bot/lib/densUtils.js
+++ bot/lib/densUtils.js
@@ -1,28 +1,46 @@
 import {
     densAPI,
-    monstersAPI
+    monstersAPI,
+    namesAPI
 } from './api.js';
 
+/**
+ * for testing
+ * 
+ * @param {*} id 
+ */
 export async function initializeDen(id) {
+    let names = testingNames();
+
+    for (let i = 0; i < 10; i++) {
+        names[i] = namesAPI.create(names[i]);
+    }
+
+    names = await Promise.all(names);
+
+    //---------------------------------
+
     let monsters = [];
 
     for (let i = 0; i < 10; i++) {
-        monsters[i] = monstersAPI.create('name', 100, 100, [
+        const name = names[i];
+
+        monsters[i] = monstersAPI.create(`${name.names.monsterNames.first[0]} ${name.names.monsterNames.middle[0]} ${name.names.monsterNames.last[0]}`, 100, 100, [
             {
                 skillNumber: 1,
-                name: 'skill 1',
+                name: name.names.skillNames[0],
                 damage: 10,
                 type: 'type 1'
             },
             {
                 skillNumber: 2,
-                name: 'skill 2',
+                name: name.names.skillNames[1],
                 damage: 20,
                 type: 'type 2'
             },
             {
                 skillNumber: 3,
-                name: 'skill 3',
+                name: name.names.skillNames[2],
                 damage: 30,
                 type: 'type 3'
             }
@@ -32,4 +50,42 @@ export async function initializeDen(id) {
     monsters = await Promise.all(monsters);
 
     densAPI.create(id, monsters);
+}
+
+/**
+ * for testing
+ * 
+ * @returns 
+ */
+function testingNames() {
+    let names = [];
+
+    for (let i = 0; i < 10; i++) {
+        let name = {
+            names:{
+                monsterNames:{
+                    first:[
+                    ],
+                    middle:[
+                    ],
+                    last:[
+                    ]
+                },
+                skillNames:[
+                ]
+            }
+        };
+
+        name.names.monsterNames.first[0] = `first${i}`;
+        name.names.monsterNames.middle[0] = `middle${i}`;
+        name.names.monsterNames.last[0] = `last${i}`;
+
+        name.names.skillNames[0] = `skill1 (${i})`;
+        name.names.skillNames[1] = `skill2 (${i})`;
+        name.names.skillNames[2] = `skill3 (${i})`;
+
+        names[i] = name;
+    }
+
+    return names;
 }
